a4229c17231c924feb354b597f95132660f7cacc
Peter Prentiss <peter.prentiss@gmail.com>
2017-06-19 23:18:38 -0500

    change for loop to forEach method and add angular to display data in dom

--- group_01.js
+++ group_01.js
@@ -7,43 +7,59 @@ var mayella = ["Mayella", "89068", "35000", 2];
 
 var employees = [atticus, jem, boo, scout, robert, mayella];
 
-
 var bonus = function (employee) {
+  //creates a loop to loop through employees array
   for (var i = 0; i < employees.length; i++) {
+    //initialize empty array to add values to
     var bonusCheck = [];
+    var name = employee[i][0];
+    var iD = employee[i][1];
     var rating = employee[i][3];
-    bonusCheck[0] = employee[i][0];
+    //assign name value to new array
+    bonusCheck[0] = name;
 
+    //check employee rating and assign bonus% value to bonusCheck array
     if (rating < 3) {
       bonusCheck[1]= 0;
-    }
-    else if (rating === 3) {
+    } else if (rating === 3) {
       bonusCheck[1]= 4;
-    }
-    else if (rating === 4) {
+    } else if (rating === 4) {
       bonusCheck[1]= 6;
-    }
-    else if (rating === 5) {
+    } else if (rating === 5) {
       bonusCheck[1]= 10;
     }
-
-   if (employee[i][1].length === 4) {
+    //check employee ID length and add 5% if length is 4
+    if (iD.length === 4) {
       bonusCheck[1] += 5;
     }
-
-   if(employee[i][2] > 65000){
+    //check if employee base salary is greater than 65000 and subtract 1% bonus if true
+    if(employee[i][2] > 65000){
         bonusCheck[1] -=1;
     }
-
-   if(bonusCheck[1]>13){
+    //check if bonus% is greater than 13 or less than 0 and set to max or min
+    if(bonusCheck[1]>13){
       bonusCheck[1]=13;
     } else if (bonusCheck[1]<0) {
     bonusCheck[1] = 0;
     }
+    //calculate bonus amount, round to nearest dollar and assign to last spot in array
+    bonusCheck[3] = Math.round(employee[i][2]*(bonusCheck[1]/100));
+    //calculate total by adding salary plus bonus amount
+    bonusCheck[2] = bonusCheck[3] + Number.parseInt(employee[i][2]);
 
-   bonusCheck [3] = Math.round(employee[i][2]*(bonusCheck[1]/100));
-
-   bonusCheck[2] = bonusCheck[3] + Number.parseInt(employee[i][2]);
+    //create text node that contains all of the values in the finished array
+    var text = document.createTextNode(
+      "Name: " + bonusCheck[0] +
+      ", Bonus Rate: " + bonusCheck[1] +
+      "%, Salary + Bonus: $" + bonusCheck[2] +
+      ", Total Bonus: $" + bonusCheck[3]
+    );
+    //create list item node to add text to
+    var par = document.createElement("li");
+    //add text node to list item node
+    var node = par.appendChild(text)
+    //add list item node to unordered list
+    document.getElementById("employee-list").appendChild(par);
 
   console.log(bonusCheck);
  }
